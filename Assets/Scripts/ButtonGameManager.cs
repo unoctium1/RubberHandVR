508782a1a501ab9044f3a9f84b93ef2f68836618
AbelW <unoctium1@gmail.com>
2019-07-10 15:53:38 -0700

    Fix buttons, add non leap version

---
+++ Assets/Scripts/ButtonGameManager.cs
@@ -0,0 +1,49 @@
+﻿using Leap.Unity.Interaction;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.UI;
+
+namespace HandVR
+{
+
+
+    public class ButtonGameManager : MonoBehaviour
+    {
+        [SerializeField]
+        private Image panel;
+        [SerializeField]
+        private Color GreenColor, RedColor;
+
+        [SerializeField]
+        private Canvas headsetCanvas;
+
+        // Start is called before the first frame update
+        void Start()
+        {
+            headsetCanvas.renderMode = RenderMode.ScreenSpaceCamera;
+            headsetCanvas.worldCamera = Camera.main;
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+
+        public void LeftButtonPressed()
+        {
+            Debug.Log("Left");
+            panel.color = RedColor;
+        }
+
+        public void RightButtonPressed()
+        {
+            Debug.Log("Right");
+            panel.color = GreenColor;
+        }
+
+
+    }
+}

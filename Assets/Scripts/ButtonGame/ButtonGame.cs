d9bbc13c42487fa1acc4f76f7023347fde1887be
AbelW <unoctium1@gmail.com>
2019-08-15 17:21:00 -0700

    Added namespace documentation

---
+++ Assets/Scripts/ButtonGame/ButtonGame.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace HandVR
+{
+    /// <summary>
+    /// Example of a custom task - ButtonGameManager implements ITest. Other scripts are utilities.
+    /// </summary>
+    namespace ButtonGame { };
+}
